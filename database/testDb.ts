752560e7ee362dfd6c1bb69314b458941258b741
nmartin <nmartin@nmartin.myguest.virtualbox.org>
2026-05-16 19:35:33 +0200

    test(script): add test script to interact with psql database via prisma in ts and schema & config ajustments

---
+++ database/testDb.ts
@@ -0,0 +1,27 @@
+import "dotenv/config"
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
+
+const prisma = new PrismaClient({ adapter })
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            email: "test.mail",
+            club: "N"
+        },
+    })
+    console.log(user)
+}
+
+main()
+    .catch(e => {
+        console.error(e.message)
+    })
+    .finally(async() => {
+        await prisma.$disconnect()
+    })
