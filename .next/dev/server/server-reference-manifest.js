630b5a965388e7b85f0ac23a61326e68504a2d67
Noé <nmartin@MacBook-Air-de-Noe.local>
2026-06-08 17:08:59 +0200

    feat(stats): elo stats evolution function

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MRow4doWC2GWdBLfHGpJluqxmWsgzxwlz9zr2QlgZVg=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__06.-pfn._.css","static/chunks/node_modules_next_dist_12hkmjb._.js","static/chunks/node_modules_react-icons_hi_index_mjs_0xb7i_s._.js","static/chunks/node_modules_react-icons_ai_index_mjs_0po2385._.js","static/chunks/node_modules_react-icons_lib_0mt5-rx._.js","static/chunks/node_modules_zustand_esm_0g.q~dc._.js","static/chunks/_0k.bar8._.js"],
-    source: "dynamic"
-});
