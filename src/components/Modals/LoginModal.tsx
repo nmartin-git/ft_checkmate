142bfd4429b61ff7a63397096f63594f9b3379dc
Marwan <maraisscode@gmail.com>
2026-06-15 17:24:44 +0200

    mid merge

--- src/components/Modals/LoginModal.tsx
+++ src/components/Modals/LoginModal.tsx
@@ -1,17 +1,15 @@
 'use client'
 
 import useLoginModal from "@/src/hooks/useLoginModal";
-// import { step, setStep, requires2FA, setRequires2FA } from "@/src/hooks/useLoginModal";
+import { step, setStep, requires2FA, setRequires2FA } from "@/src/hooks/useLoginModal";
 import Input from "../ui/Input";
 import { useState, useCallback } from "react";
 import Modal from "../ui/Modal";
 import useRegisterModal from "@/src/hooks/useRegisterModal";
 import useCurrentUser from "@/src/hooks/useCurrentUser";
 
-
-
 const LoginModal= () => {
-    const LoginModal = useLoginModal();
+    const { step, setStep, requires2FA, setRequires2FA, isOpen, onClose } = useLoginModal();
     const registerModal = useRegisterModal();
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
@@ -24,8 +22,8 @@ const LoginModal= () => {
             LoginModal.onClose();
             registerModal.onOpen();
         },[registerModal, LoginModal, isLoading]);
-    // if (step === 1)
-    // {
+    if (step === 1)
+    {
          const onSubmit =useCallback(async () =>{
             try{
                 setIsLoading(true);
@@ -47,11 +45,11 @@ const LoginModal= () => {
                     username : data.user.username,
                     email : data.user.email
                 })
-                // if (data.twoFactorAuthEnable)
-                // {
-                //     setStep(2);
-                //     return ;
-                // }
+                if (data.twoFactorAuthEnable)
+                {
+                    setStep(2);
+                    return ;
+                }
                 LoginModal.onClose();
             } catch (error: any)
             {
@@ -60,8 +58,7 @@ const LoginModal= () => {
                 setIsLoading(false);    
             }
 
-        }, [currentUser,LoginModal, email, password]);
-        const bodyContent = (
+        bodyContent = (
             <div className="flex flex-col gap-4">
                 <Input
                 placeholder="Email"
@@ -79,7 +76,7 @@ const LoginModal= () => {
                 />
             </div>
         )
-        const footerContent = (
+        footerContent = (
            <div className="flex flex-row py-2">
             <p className="pr-2">Dont have an account ? </p>
             <span className="
@@ -104,75 +101,75 @@ const LoginModal= () => {
             />
         </div>
         );
-    // // }
-    // // else if (step === 2)
-    // // {
-    //     const onSubmit =useCallback(async () =>{
-    //         try{
-    //             setIsLoading(true);
-    //             const response = await fetch ('api/auth/login', {
-    //                 method : 'POST',
-    //                 headers : {
-    //                     'Content-Type' : 'application/json'
-    //                 },
-    //                 body : JSON.stringify({
-    //                     code
-    //                 })
-    //             });
-    //             const data = await response.json();
-    //             if (!response.ok)
-    //                 throw new Error(data.error || 'response pas ok');
-    //             alert('Utilisateur log avec succes!');
-    //             currentUser.setUser({
-    //                 id : data.user.id,
-    //                 username : data.user.username,
-    //                 email : data.user.email
-    //             })
-    //             LoginModal.onClose();
-    //         } catch (error: any)
-    //         {
-    //             console.log(error.message);
-    //         } finally {
-    //             setIsLoading(false);    
-    //         }
+    }
+    else if (step === 2)
+    {
+        const onSubmit =useCallback(async () =>{
+            try{
+                setIsLoading(true);
+                const response = await fetch ('api/auth/login', {
+                    method : 'POST',
+                    headers : {
+                        'Content-Type' : 'application/json'
+                    },
+                    body : JSON.stringify({
+                        code
+                    })
+                });
+                const data = await response.json();
+                if (!response.ok)
+                    throw new Error(data.error || 'response pas ok');
+                alert('Utilisateur log avec succes!');
+                currentUser.setUser({
+                    id : data.user.id,
+                    username : data.user.username,
+                    email : data.user.email
+                })
+                LoginModal.onClose();
+            } catch (error: any)
+            {
+                console.log(error.message);
+            } finally {
+                setIsLoading(false);    
+            }
 
-    //     }, [currentUser,LoginModal, email, password]);
-    //     const bodyContent = (
-    //         <div className="flex flex-col gap-4">
-    //             <Input
-    //             placeholder="code"
-    //             onChange={(e)=>setCode(e.target.value)}
-    //             value={code}
-    //             disabled={isLoading}
+        }, [currentUser,LoginModal, email, password]);
+        const bodyContent = (
+            <div className="flex flex-col gap-4">
+                <Input
+                placeholder="code"
+                onChange={(e)=>setCode(e.target.value)}
+                value={code}
+                disabled={isLoading}
 
-    //             />
-    //         </div>
-    //     )
-    //     const footerContent = (
-    //         <div className="flex flex-row py-2">
-    //         <p className="pr-2">Have a problem with two factor authentification ? </p>
-    //         <span className="
-    //         text-decoration-line: underline
-    //         cursor-pointer 
-    //         hover:opacity-50
-    //         " onClick={onToggle}>Recovery codes</span>
-    //         </div>
-    //     )
-    //     return (
-    //     <div>
-    //         <Modal
-    //         disabled={isLoading}
-    //         isOpen={LoginModal.isOpen}
-    //         title="Login"
-    //         actionLabel="Sign in"
-    //         onClose={LoginModal.onClose}
-    //         onSubmit={onSubmit}
-    //         body={bodyContent}
-    //         footer={footerContent}
+                />
+            </div>
+        )
+        const footerContent = (
+            <div className="flex flex-row py-2">
+            <p className="pr-2">Have a problem with two factor authentification ? </p>
+            <span className="
+            text-decoration-line: underline
+            cursor-pointer 
+            hover:opacity-50
+            " onClick={onToggle}>Recovery codes</span>
+            </div>
+        )
+        return (
+        <div>
+            <Modal
+            disabled={isLoading}
+            isOpen={LoginModal.isOpen}
+            title="Login"
+            actionLabel="Sign in"
+            onClose={LoginModal.onClose}
+            onSubmit={onSubmit}
+            body={bodyContent}
+            footer={footerContent}
 
-    //         />
-    //     </div>
-    //     );
-    // // }
+            />
+        </div>
+        );
+    }
 }
 export default LoginModal
